9d10a6341a08f7fe549f66c7af4f7da275f6de0d
Stas <www.stas1996@mail.ru>
2023-04-25 09:42:54 +0300

    добавил проверку массива на заданное условие

--- Program.cs
+++ Program.cs
@@ -10,7 +10,17 @@ string[] CheckArray(string[] array)
 
 int CheckNumbersArray(string[] array)
 { 
-
+int CheckNumbersArray(string[] array)
+{ 
+    int count = 0;
+for(int i = 0;  i < array.Length; i++)
+    { 
+        if (array[i].Length < 4)
+        {
+            count++;
+        }
+    }
+return count;
 }
 
 Console.Write("Введите количество элементов массива: ");
