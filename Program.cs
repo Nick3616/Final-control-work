39cc40162a6c94adde6bca76bcf14e886d623400
Stas <www.stas1996@mail.ru>
2023-04-25 09:30:43 +0300

    добавил шапку

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿string[] ImputArray(int length)
+{
+
+}
+
+string[] CheckArray(string[] array)
+{ 
+
+}
+
+int CheckNumbersArray(string[] array)
+{ 
+
+}
+
+Console.Write("Введите количество элементов массива: ");
+int length = int.Parse(Console.ReadLine()!);
+if(length < 0)
+{
+    Console.Write("Вы ошиблись!\nВведиет положительное количество элементов массива :");
+    length = int.Parse(Console.ReadLine()!);
+}
+
